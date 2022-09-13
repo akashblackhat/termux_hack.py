aa9ce2201159dc3ee6cc94999936388ac0015b19
AkashBlackHat <88341460+akashblackhat@users.noreply.github.com>
2022-09-13 17:55:57 +0530

    one click all pkg install

---
+++ termux_hack.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+import os
+import time
+import sys
+#HACKER AKASH BLACK HAT
+os.system("clear")
+print('''\033[91m
+  ),\                                                           /,( 
+   /__'.                                                      .'/__
+    `)   `'-. \             AKASH BLACK HAT             / .-'`   ('
+    /   _.--'\ '.          ,               ,          .' /'--._     |
+    \         _.`'-.,_'-.|/\ \    _,_    / /\|.-'_,.-'`._         /
+     `\    .-'       /'-.|| \ |.-"   "-.| / ||.-'\       '-.    /`
+       )-'`        .'   :||  / -.\ //.- \  ||:   '.        `'-(
+      /          .'    / \_ |  /●`^'●\  | _// \    '.        )
+       `)  _.'     .'    .--.; |\__"__/| ;.--.    '.     '._  ('
+       /_.'     .-'  _.-'     \ \/^\/ //     `-._  '-.     '.
+        '-._' /`            _   \-.-//   _            `\ '_.-'
+            `<     _,..--''`|    \`"`/    |`''--..,_     >`
+             _\  ``--..__   \     `'`     /   __..--``  /_
+            /  '-.__     ``'-;    / \    ;-'``     __.-'
+            '-._    '-._  /    |---|---|    \  _.-'    _.-'
+                 `'-._   '/ / / /---|---\ \ \ '   _.-'` 
+                         `)` `  /'---'\  ` `(`               
+                  /_____/|  / \._\   /_./ \  |\_____                                                               
+                               version:3.0
+       ''')
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+
+print(''' \033[95m
+   +--------------------------------------+
+   | This Tool Install All Basic Packages |
+   +--------------------------------------+
+   | Coded By: AKASH BLACK HAT            |
+   +--------------------------------------+
+   | YouTube : shorturl.at/hrsTY          |                           
+   +--------------------------------------+
+   | Facebook: shorturl.at/ckqW5          |
+   +--------------------------------------+
+   | GitHub  : akashblackhat GitHub       |
+   +--------------------------------------+''')
+
+slowprint(''' \033[93m
+[01] python             [02] python2
+[03] python-dev         [04] python3
+[05] php                [06] java
+[07] git                [08] perl
+[09] bash               [10] nano
+[11] curl               [12] openssl
+[13] openssh            [14] wget
+[15] clang              [16] nmap
+[17] w3m                [18] hydra
+[19] ruby               [20] macchanger
+[21] host               [22] dnsutils
+[23] coreutils          [24] fish
+[25] zip                [27] tor
+[28] hydra              [29] figlet 
+[30] cowsay             [31] tar
+[32] unzip              [33] vim
+[34] ruby               [35] wcalc
+[36] bmon               [37] unrar
+[38] proot              [39] golang
+[40] NsLookup''')
+slowprint('''\033[96m
+This Command for access Storage in Termux
+[00] termux-setup-storage''')
+print("                                            ")
+choice = input("\033[93mDo You Want to Install All Packages [y/n] : ")
+if choice == 'n': sys.exit()
+if choice == 'y': os.system("apt upgrade -y > /dev/null 2>&1")
+os.system("pkg update -y >/dev/null")
+os.system("pkg install python -y > /dev/null 2>&1")
+os.system("pkg install python2 -y > /dev/null 2>&1")
+os.system("pkg install php -y > /dev/null 2>&1")
+os.system("pkg install python-dev -y > /dev/null 2>&1")
+os.system("pkg install python3 -y > /dev/null 2>&1")
+os.system("pkg install java -y > /dev/null 2>&1")
+os.system("pkg install root-repo -y > /dev/null 2>&1")
+os.system("pkg install unstable-repo -y > /dev/null 2>&1")
+os.system("pkg install x11-repo -y > /dev/null 2>&1")
+os.system("pkg install git -y > /dev/null 2>&1")
+os.system("pkg install perl -y > /dev/null 2>&1")
+os.system("pkgy install bash -y > /dev/null 2>&1")
+
+print("Wait For A Little Bit")
+
+os.system("pip2 install lolcat requests mechanize bs4 > /dev/null 2>&1")
+os.system("apt install nano -y > /dev/null 2>&1")
+os.system("apt install curl -y > /dev/null 2>&1")
+os.system("apt install openssl -y > /dev/null 2>&1")
+os.system("apt install openssh -y > /dev/null 2>&1")
+os.system("apt install wget -y > /dev/null 2>&1")
+os.system("apt install clang -y > /dev/null 2>&1")
+os.system("apt install nmap -y > /dev/null 2>&1")
+os.system("apt install w3m -y > /dev/null 2>&1")
+os.system("apt install hydra -y > /dev/null 2>&1")
+
+print("""subscribe  TECHNICAL AKASH SKILLS""")
+os.system("am start -a android.intent.action.VIEW -d https://www.youtube.com/channel/UCWlAUmBOM07RXwwfZyfj_uw > /dev/null 2>&1")
+
+os.system("apt install ruby2 -y > /dev/null 2>&1")
+os.system("apt install macchanger -y > /dev/null 2>&1")
+os.system("apt install host -y > /dev/null 2>&1")
+os.system("apt install dnsutils -y > /dev/null 2>&1")
+os.system("apt install coreutils -y > /dev/null 2>&1")
+os.system("apt install fish -y > /dev/null 2>&1")
+os.system("apt install zip -y > /dev/null 2>&1")
+os.system("apt install hydra -y > /dev/null 2>&1")
+os.system("apt install figlet -y > /dev/null 2>&1")
+os.system("apt install cowsay -y > /dev/null 2>&1")
+os.system("apt install unzip -y > /dev/null 2>&1")
+os.system("apt install vim -y > /dev/null 2>&1")
+os.system("apt install ruby -y > /dev/null 2>&1")
+os.system("apt install wcalc -y > /dev/null 2>&1")
+os.system("apt install bmon -y > /dev/null 2>&1")
+os.system("apt install unrar -y > /dev/null 2>&1")
+os.system("apt install proot -y > /dev/null 2>&1")
+os.system("apt install golang -y > /dev/null 2>&1")
+os.system("pkg install dnsutils -y > /dev/null 2>&1")
+print("Allow the Button For Access the Storage in Termux")
+
+os.system("termux-setup-storage")
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+
+
+print("\033[95m+-------------------------------------------------------+")
+slowprint('''\033[95m|     Welcome To Hacker:AkashBlackHat                   |
+|Subscribe Our YouTube Channel: technical akash skills  |
+|                                                       | ''')
+print("+-------------------------------------------------------+")
+
+input("\n\nPress the enter key to exit : ")
+#thank you hackers
